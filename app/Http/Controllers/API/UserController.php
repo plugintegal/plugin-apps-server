62fa0055bf4522b1b72a7b8c145e198ad3613810
royhan31 <izzapark34@gmail.com>
2019-09-25 23:38:15 +0700

    first commit

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+
+class UserController extends Controller
+{
+    public function register(Request $request){
+      $rule = [
+        'name' => 'required',
+        'email' => 'required|email|unique:users'
+      ];
+      $message = [
+        'required' => 'isi bidang ini.',
+        'email' => 'Masukan email dengan benar'
+      ];
+      $this->validate($request, $rule, $message);
+    }
+}
