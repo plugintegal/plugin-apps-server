3e7bce7dc56b8991fa0fd5afca63bfdfa0c37c19
royhan31 <izzapark34@gmail.com>
2019-12-29 22:43:41 +0700

    fix user personal

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use Illuminate\Http\Request;
 
 Route::post('register','User\AuthUserController@register');
 Route::post('login','User\AuthUserController@login');
-Route::get('user/profile','User\UserController@profile')->middleware('auth:api');
 Route::get('user/{member_id}','User\UserController@show');
 Route::get('users','User\UserController@index');
+Route::get('user/profile','User\UserController@profile')->middleware('auth:api');
 Route::put('user/profile', 'User\PersonalController@updatePersonal')->middleware('auth:api');
 
 
